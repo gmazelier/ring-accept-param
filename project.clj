5bc5a64a136bd497386d68ea46bca42d8ba4d41b
Gaylord Mazelier <gaylord@mazelier.fr>
2012-03-11 11:25:20 +0100

    Start working on version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject ring-accept-param "0.0.1"
+(defproject ring-accept-param "0.1.0-SNAPSHOT"
   :description "Ring middleware that augments :params according to the specified Accept request-header field."
   :dependencies [[org.clojure/clojure "1.3.0"]])
