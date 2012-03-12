9048df6473b8e3a2e9a3be0fe536c90512de38a1
Gaylord Mazelier <gaylord@mazelier.fr>
2012-03-12 12:10:18 +0100

    Add project url.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ring-accept-param "0.1.0-SNAPSHOT"
   :description "Ring middleware that augments :params according to the specified Accept request-header field."
+  :url "https://github.com/gmazelier/ring-accept-param"
   :dependencies [[org.clojure/clojure "1.3.0"]])
