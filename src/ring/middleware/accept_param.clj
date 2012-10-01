92c7533c505ad10851c207d6c798e0ef9b42ca9d
Gaylord Mazelier <gaylord@mazelier.fr>
2012-10-01 22:02:54 +0200

    Add CSV and plain formats.

--- src/ring/middleware/accept_param.clj
+++ src/ring/middleware/accept_param.clj
@@ -2,53 +2,55 @@
 
 (def default-format :html)
 
-(def formats {:json "^application/json"
-	          :html "^text/html"
-	          :xml "^text/xml"})
+(def formats {:json  "^application/json"
+              :html  "^text/html"
+              :xml   "^text/xml"
+              :csv   "^text/csv"
+              :plain "^text/plain"})
 
 (defn accept-format?
-	"Returns true if value match the given pattern, false or nil otherwise."
-	{ :added "0.0.1" }
-	[value pattern]
-	(when-not (nil? value)
-		(not
-			(empty?
-				(re-seq (re-pattern pattern) value)))))
+  "Returns true if value match the given pattern, false or nil otherwise."
+  { :added "0.0.1" }
+  [value pattern]
+  (when-not (nil? value)
+    (not
+      (empty?
+        (re-seq (re-pattern pattern) value)))))
 
 (defn match-accept
-	"Returns matching entry from formats map given accept-header value."
-	{ :added "0.0.1" }
-	[accept-header]
-	(select-keys formats
-		(for [[format-keyword format-pattern]
-			  formats
-			  :when (accept-format? accept-header format-pattern)]
-			format-keyword)))
+  "Returns matching entry from formats map given accept-header value."
+  { :added "0.0.1" }
+  [accept-header]
+  (select-keys formats
+    (for [[format-keyword format-pattern]
+          formats
+          :when (accept-format? accept-header format-pattern)]
+      format-keyword)))
 
 (defn extract-first-format
-	"Returns first keyword as accepted format if exists, default value otherwise.
+  "Returns first keyword as accepted format if exists, default value otherwise.
 
-	At this point, accept-map should be empty or contain one entry. If accept-map cointains more
-	than one entry, keys are sorted by ascending value and the first one is returned."
-	{ :added "0.0.1" }
-	[accept-map]
-	(if (empty? accept-map)
-		(name default-format)
-		(when-first [first-format (keys (into (sorted-map) accept-map))]
-			(name first-format))))
+  At this point, accept-map should be empty or contain one entry. If accept-map cointains more
+  than one entry, keys are sorted by ascending value and the first one is returned."
+  { :added "0.0.1" }
+  [accept-map]
+  (if (empty? accept-map)
+    (name default-format)
+    (when-first [first-format (keys (into (sorted-map) accept-map))]
+      (name first-format))))
 
 (defn detect-accept
-	"Builds the map based on Accept request-header field analysis."
-	{ :added "0.0.1" }
-	[accept-header]
-	{:accept (extract-first-format (match-accept accept-header))})
+  "Builds the map based on Accept request-header field analysis."
+  { :added "0.0.1" }
+  [accept-header]
+  {:accept (extract-first-format (match-accept accept-header))})
 
 (defn wrap-accept-param
-	"Augments :params according to the specified Accept request-header field."
-	{ :added "0.0.1" }
-	[handler]
-	(fn [req]
-		(let [#^String accept-header (get (:headers req) "accept")
-			           accept        (detect-accept accept-header)
-			           req*          (assoc req :params (merge accept (:params req)))]
-			(handler req*))))
+  "Augments :params according to the specified Accept request-header field."
+  { :added "0.0.1" }
+  [handler]
+  (fn [req]
+    (let [#^String accept-header (get (:headers req) "accept")
+                   accept        (detect-accept accept-header)
+                   req*          (assoc req :params (merge accept (:params req)))]
+      (handler req*))))
--- test/ring/middleware/accept_param_tests.clj
+++ test/ring/middleware/accept_param_tests.clj
@@ -1,107 +1,113 @@
 (ns ring.middleware.accept-param-tests
-	(:use clojure.test)
-	(:require [ring.middleware.accept-param :as mdw]))
+  (:use clojure.test)
+  (:require [ring.middleware.accept-param :as mdw]))
 
 ; test accept-format?
 (deftest is-json
-	(is (mdw/accept-format? "application/json" "^application/json")))
+  (is (mdw/accept-format? "application/json" "^application/json")))
 
 (deftest xml-is-not-json
-	(is (not (mdw/accept-format? "text/xml" "^application/json"))))
+  (is (not (mdw/accept-format? "text/xml" "^application/json"))))
 
 (deftest empty-string-is-not-json
-	(is (not (mdw/accept-format? "" "^application/json"))))
+  (is (not (mdw/accept-format? "" "^application/json"))))
 
 (deftest nil-is-not-json
-	(is (not (mdw/accept-format? nil "^application/json"))))
+  (is (not (mdw/accept-format? nil "^application/json"))))
 
 ; test match-accept
 (deftest match-nil
-	(is (= {} (mdw/match-accept nil))))
+  (is (= {} (mdw/match-accept nil))))
 
 (deftest match-empty-string
-	(is (= {} (mdw/match-accept ""))))
+  (is (= {} (mdw/match-accept ""))))
 
 (deftest match-unsupported-format
-	(is (= {} (mdw/match-accept "audio/mpeg"))))
+  (is (= {} (mdw/match-accept "audio/mpeg"))))
 
 (deftest match-json-format
-	(is (= {:json "^application/json"} (mdw/match-accept "application/json"))))
+  (is (= {:json "^application/json"} (mdw/match-accept "application/json"))))
 
 (deftest match-html-format
-	(is (= {:html "^text/html"} (mdw/match-accept "text/html;level=1"))))
+  (is (= {:html "^text/html"} (mdw/match-accept "text/html;level=1"))))
 
 (deftest match-xml-format
-	(is (= {:xml "^text/xml"} (mdw/match-accept "text/xml;q=0.8"))))
+  (is (= {:xml "^text/xml"} (mdw/match-accept "text/xml;q=0.8"))))
+
+(deftest match-csv-format
+  (is (= {:csv "^text/csv"} (mdw/match-accept "text/csv"))))
+
+(deftest match-plain-format
+  (is (= {:plain "^text/plain"} (mdw/match-accept "text/plain"))))
 
 ; test extract-first-format
 (deftest extract-from-nil
-	(is (= "html" (mdw/extract-first-format nil))))
+  (is (= "html" (mdw/extract-first-format nil))))
 
 (deftest extract-from-emtpy-map
-	(is (= "html" (mdw/extract-first-format {}))))
+  (is (= "html" (mdw/extract-first-format {}))))
 
 (deftest extract-from-one-entry-map
-	(is (= "xml" (mdw/extract-first-format {:xml "^text/xml"}))))
+  (is (= "xml" (mdw/extract-first-format {:xml "^text/xml"}))))
 
 (deftest extract-from-multiple-entries-map
-	(is (= "html" (mdw/extract-first-format {:html "^text/html" :json "^application/json" :xml "^text/xml"}))))
+  (is (= "html" (mdw/extract-first-format {:html "^text/html" :json "^application/json" :xml "^text/xml"}))))
 
 (deftest extract-from-multiple-entries-map-inversed
-	(is (= "html" (mdw/extract-first-format {:xml "^text/xml" :json "^application/json" :html "^text/html"}))))
+  (is (= "html" (mdw/extract-first-format {:xml "^text/xml" :json "^application/json" :html "^text/html"}))))
 
 ; test augments-with-accept-json
 (def echo-wrapper
-	(mdw/wrap-accept-param identity))
+  (mdw/wrap-accept-param identity))
 
 (deftest augments-with-accept-json
-	(let [req {:headers {"accept" "application/json"}
-			   :params  {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
+  (let [req {:headers {"accept" "application/json"}
+         :params  {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
 
 (deftest augments-with-accept-json-and-level
-	(let [req {:headers {"accept" "application/json;level=1"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
+  (let [req {:headers {"accept" "application/json;level=1"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
 
 (deftest augments-with-accept-json-and-quality
-	(let [req {:headers {"accept" "application/json;q=0.8"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
+  (let [req {:headers {"accept" "application/json;q=0.8"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
 
 (deftest augments-with-accept-json-and-star
-	(let [req {:headers {"accept" "application/json,*/*"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
+  (let [req {:headers {"accept" "application/json,*/*"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
 
 (deftest augments-with-accept-json-first
-	(let [req {:headers {"accept" "application/json,text/xml,text/html"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
+  (let [req {:headers {"accept" "application/json,text/xml,text/html"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "json"} (:params resp)))))
 
 (deftest augments-with-accept-xml
-	(let [req {:headers {"accept" "text/xml"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "xml"} (:params resp)))))
+  (let [req {:headers {"accept" "text/xml"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "xml"} (:params resp)))))
 
 (deftest augments-with-accept-html
-	(let [req {:headers {"accept" "text/html"}
-			   :params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
+  (let [req {:headers {"accept" "text/html"}
+         :params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
 
 (deftest augments-with-accept-html-if-no-accept-request-header
-	(let [req {:params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
+  (let [req {:params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
 
 (deftest augments-with-accept-html-if-unrecognized-accept-request-header
-	(let [req {:params {"id" "4f4ceffde4b0e75979becd25"}}
-		  resp (echo-wrapper req)]
-		(is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
+  (let [req {:params {"id" "4f4ceffde4b0e75979becd25"}}
+      resp (echo-wrapper req)]
+    (is (= {"id" "4f4ceffde4b0e75979becd25" :accept "html"} (:params resp)))))
